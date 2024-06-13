7f48395b78455368de7c89e0513b3dc3593168de
loevray <rei980213@gmail.com>
2024-06-13 16:17:24 +0900

    Style: Add arrow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,11 @@ const Home = () => {
   return (
     <main className="w-screen h-screen flex items-center">
       <section className="pl-[50px] w-1/2">
-        <article>
-          <SpinWheel
-            diameter={DEFAULT_VALUES.CIRCLE_DIAMETER} //지름은 무조건 4의 배수여야 함
-            onStartRotate={() => console.log("돌림판 시작")}
-            onStopRotate={() => console.log("돌림판 멈춤")}
-          />
-        </article>
+        <SpinWheel
+          diameter={DEFAULT_VALUES.CIRCLE_DIAMETER} //지름은 무조건 4의 배수여야 함
+          onStartRotate={() => console.log("돌림판 시작")}
+          onStopRotate={() => console.log("돌림판 멈춤")}
+        />
       </section>
       <section className="w-1/2">
         <article className="flex flex-col gap-5">
--- src/app/ui/main/SpinWheel.tsx
+++ src/app/ui/main/SpinWheel.tsx
@@ -17,21 +17,33 @@ const SpinWheel = ({
   const totalSector = sectorData.reduce((acc, cur) => acc + cur.ratio, 0);
 
   return (
-    <svg
-      viewBox={`0 0 ${diameter} ${diameter}`}
-      width={diameter}
-      height={diameter}
-      transform="rotate(-90)"
+    <figure
+      className="relative"
+      style={{
+        width: `${diameter}px`,
+        height: `${diameter}px`,
+      }}
     >
-      {sectorData.map((data) => (
-        <SpinWheelSector
-          key={data.id}
-          radius={diameter / 4}
-          totalSector={totalSector}
-          {...data}
-        />
-      ))}
-    </svg>
+      <span
+        className="bg-blue-500 w-10 h-10 absolute left-1/2 translate-x-[-50%] z-10"
+        style={{ clipPath: "polygon(50% 100%, 25% 0, 75% 0)" }}
+      ></span>
+      <svg
+        viewBox={`0 0 ${diameter} ${diameter}`}
+        width={diameter}
+        height={diameter}
+        transform="rotate(-90)"
+      >
+        {sectorData.map((data) => (
+          <SpinWheelSector
+            key={data.id}
+            radius={diameter / 4}
+            totalSector={totalSector}
+            {...data}
+          />
+        ))}
+      </svg>
+    </figure>
   );
 };
 
