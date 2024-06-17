a49b9820bc065ca455dc508cd82b6770cd2eb283
loevray <rei980213@gmail.com>
2024-06-17 23:59:53 +0900

    Style: nav bg color changes depend on  path

--- src/app/ui/Navigation.tsx
+++ src/app/ui/Navigation.tsx
@@ -1,19 +1,55 @@
-import Link from "next/link";
+"use client";
 
-const links = {
-  "/": "메인",
-  "spin-wheel": "룰렛",
-  "youtube-comment-raffle": "유튜브 댓글 추첨",
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export type RoutesType = "/" | "spin-wheel" | "youtube-comment-raffle";
+
+interface I_RoutesStyle {
+  name: string;
+  backgroundColor: string;
+}
+type MappedRoutesStyleType = {
+  [key in RoutesType]: I_RoutesStyle;
 };
-const linksArray = Object.entries(links);
+
+const mappedRoutesStyle: MappedRoutesStyleType = {
+  "/": {
+    name: "메인",
+    backgroundColor: "bg-gray",
+  },
+  "spin-wheel": {
+    name: "룰렛",
+    backgroundColor: "bg-cyan-300",
+  },
+  "youtube-comment-raffle": {
+    name: "유튜브 댓글 추첨",
+    backgroundColor: "bg-yellow-300",
+  },
+};
+
+const mappedRoutesStyleArray = Object.entries(mappedRoutesStyle);
+
+const isCorrectRoute = (
+  currentPath: string,
+  routes: MappedRoutesStyleType
+): currentPath is keyof MappedRoutesStyleType => currentPath in routes;
 
 const Navigation = () => {
+  const pathname = usePathname();
+  const currentPath = pathname.split("/")[1];
+  const currentBackgroundColor = isCorrectRoute(currentPath, mappedRoutesStyle)
+    ? mappedRoutesStyle[currentPath].backgroundColor
+    : "bg-black";
+
   return (
-    <nav className="sticky top-0 bg-teal-600 h-screen w-20">
+    <nav className={`sticky top-0  h-screen w-20 ${currentBackgroundColor}`}>
       <ul>
-        {linksArray.map(([link, name]) => (
+        {mappedRoutesStyleArray.map(([link, { name }]) => (
           <li key={link}>
-            <Link href={link}>{name}</Link>
+            <Link href={link} className="text-white">
+              {name}
+            </Link>
           </li>
         ))}
       </ul>
