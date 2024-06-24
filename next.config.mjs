a347b8e9032d738b7218a5b2bb0504cccbb0e1ef
loevray <rei980213@gmail.com>
2024-06-24 20:24:04 +0900

    Chore: add next config image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+        pathname: "/vi/**",
+      },
+    ],
+  },
+};
+("https://i.ytimg.com/vi/ZHaOU6E4pWU/sddefault.jpg");
 export default nextConfig;
