8761aa43d5fbf6c856dec4838c6176c9932489b8
loevray <rei980213@gmail.com>
2024-06-25 04:44:19 +0900

    Refactor: abstract CommentTypeForm

---
+++ src/app/ui/youtube-comment-raflle/CommentTypeForm.tsx
@@ -0,0 +1,46 @@
+import Button from "../common/Button";
+
+interface CommentTypeFormProps {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  commentType: CommentType;
+}
+
+export type CommentType = {
+  thread: boolean;
+  reply: boolean;
+};
+
+const CommentTypeForm = ({ onChange, commentType }: CommentTypeFormProps) => {
+  return (
+    <form className="flex gap-2">
+      <div>
+        <input
+          className="h-1.5"
+          type="radio"
+          name="youtube-comments"
+          value="thread"
+          onChange={onChange}
+          defaultChecked={commentType.thread}
+          id="thread"
+        />
+        <label htmlFor="thread">댓글 목록</label>
+      </div>
+      <div>
+        <input
+          className="h-1.5"
+          type="radio"
+          name="youtube-comments"
+          data-name="comments"
+          value="reply"
+          onChange={onChange}
+          defaultChecked={commentType.reply}
+          id="reply"
+        />
+        <label htmlFor="reply">답글 목록</label>
+      </div>
+      <Button type="button" text="불러오기" colorPalette="rin" />
+    </form>
+  );
+};
+
+export default CommentTypeForm;
--- src/app/youtube-comment-raffle/page.tsx
+++ src/app/youtube-comment-raffle/page.tsx
@@ -12,43 +12,49 @@ import VideoInfo from "../ui/youtube-comment-raflle/VideoInfo";
 import { CustomCommentDataType } from "@/utils/parsedYoutubeCommentThread";
 import Comment from "../ui/youtube-comment-raflle/Comment";
 import YoutubeLinkForm from "../ui/youtube-comment-raflle/YoutubeLinkForm";
+import CommentTypeForm, {
+  CommentType,
+} from "../ui/youtube-comment-raflle/CommentTypeForm";
+
+const DEFAULT_VIDEO_CUSTOM_DATA = {
+  title: "영상 제목",
+  channelTitle: "채널 이름",
+  commentCount: 0,
+  viewCount: 0,
+  thumbnail: {
+    url: "https://placehold.co/400x200/png",
+    width: 500,
+    height: 300,
+  },
+};
 
 const Page = () => {
   const youtubeInputRef = useRef<HTMLInputElement>(null);
   const [comments, setComments] = useState<CustomCommentDataType[]>([]);
-  const [videoData, setVideoData] = useState<YoutubeVideoCustomData>({
-    title: "영상 제목",
-    channelTitle: "채널 이름",
-    commentCount: 0,
-    viewCount: 0,
-    thumbnail: {
-      url: "https://placehold.co/600x400/png",
-      width: 500,
-      height: 300,
-    },
-  });
-  const [commentListMode, setCommentListMode] = useState<{
-    thread: boolean;
-    reply: boolean;
-  }>({
+  const [videoData, setVideoData] = useState<YoutubeVideoCustomData>(
+    DEFAULT_VIDEO_CUSTOM_DATA
+  );
+  const [commentType, setCommentType] = useState<CommentType>({
     thread: true,
     reply: false,
   });
 
-  const onRadioButtonChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onCommentTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
 
-    setCommentListMode((prev) => ({ ...prev, [value]: true }));
+    setCommentType((prev) => ({ ...prev, [value]: true }));
   };
 
   const raffleComment = () => {
-    if (videoData && comments.length) {
-      const maxNumber = videoData.commentCount;
-      const [winnerIndex] = raffle({ low: 1, high: maxNumber });
-      const winner = comments[winnerIndex];
-      const { authorDisplayName, textOriginal } = winner;
-      alert(`${authorDisplayName}: ${textOriginal}`);
-    }
+    if (!videoData) throw new Error("비디오 데이터가 없어요!");
+    if (!comments.length) throw new Error("댓글 데이터가 없어요!");
+
+    const maxNumber = videoData.commentCount;
+    const [winnerIndex] = raffle({ low: 1, high: maxNumber });
+
+    const winner = comments[winnerIndex];
+    const { authorDisplayName, textOriginal } = winner;
+    alert(`${authorDisplayName}: ${textOriginal}`);
   };
 
   const handleSubmitYoutubeLink =
@@ -81,39 +87,13 @@ const Page = () => {
             }
           />
         </div>
-
         <div className="w-full flex items-center gap-6">
           <h1 className="text-2 font-bold">2. 댓글 유형 선택</h1>
-          <form className="flex gap-2">
-            <div>
-              <input
-                className="h-1.5"
-                type="radio"
-                name="youtube-comments"
-                value="thread"
-                onChange={onRadioButtonChange}
-                defaultChecked={commentListMode.thread}
-                id="thread"
-              />
-              <label htmlFor="thread">댓글 목록</label>
-            </div>
-            <div>
-              <input
-                className="h-1.5"
-                type="radio"
-                name="youtube-comments"
-                data-name="comments"
-                value="reply"
-                onChange={onRadioButtonChange}
-                defaultChecked={commentListMode.reply}
-                id="reply"
-              />
-              <label htmlFor="reply">답글 목록</label>
-            </div>
-            <Button type="button" text="불러오기" colorPalette="rin" />
-          </form>
+          <CommentTypeForm
+            onChange={onCommentTypeChange}
+            commentType={commentType}
+          />
         </div>
-
         <div className="w-full flex items-center gap-6">
           <h1 className="text-2 font-bold">3. 추첨 방식 결정</h1>
           <form>
