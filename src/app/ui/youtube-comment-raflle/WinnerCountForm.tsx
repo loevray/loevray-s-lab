fa8b9f45902fdf8340dbe09b6c2c58d01d30e5bd
loevray <rei980213@gmail.com>
2024-06-26 23:53:49 +0900

    Fix: WinnerCountForm string bug fix

--- src/app/ui/youtube-comment-raflle/WinnerCountForm.tsx
+++ src/app/ui/youtube-comment-raflle/WinnerCountForm.tsx
@@ -12,7 +12,7 @@ const WinnerCountForm = ({
       className="w-7 h-3 pl-1 shadow-xl bg-yellow-50 focus:bg-yellow-100 focus:outline-none"
       type="number"
       value={winnerLimitState}
-      onChange={(e) => onChange(e.target.value)}
+      onChange={(e) => onChange(+e.target.value)}
     />
     명
   </form>
