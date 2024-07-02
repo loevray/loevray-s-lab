fb45a2681d744d719e1917893a6adbd052e98b66
loevray <rei980213@gmail.com>
2024-07-02 17:51:42 +0900

    Feat: add LinearProgress component

---
+++ src/app/ui/common/progress/LinearProgress.tsx
@@ -0,0 +1,25 @@
+import { ComponentProps } from "react";
+
+interface LinearProgressProps extends ComponentProps<"span"> {
+  value: number;
+}
+
+const LinearProgress = ({
+  value,
+
+  ...rest
+}: LinearProgressProps) => {
+  const currentX = -Math.max(100 - value, 0).toFixed(1);
+  return (
+    <span {...rest} className="w-full relative overflow-hidden bg-cyan-200">
+      <span
+        className="bg-cyan-500 absolute left-0 top-0 w-full ease-linear h-full transition-transform"
+        style={{
+          transform: `translateX(${currentX}%)`,
+        }}
+      ></span>
+    </span>
+  );
+};
+
+export default LinearProgress;
