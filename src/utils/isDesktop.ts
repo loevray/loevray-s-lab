e535f5eaca9ce651b6bcdc27c2cf6cb1069dbf08
loevray <rei980213@gmail.com>
2024-07-03 18:39:36 +0900

    Fix: window not defined bug fix

--- src/utils/isDesktop.ts
+++ src/utils/isDesktop.ts
@@ -1,9 +1,9 @@
 const isDesktop = () => {
-  if(typeof window !== undefined){
+  if(typeof window !== 'undefined'){
     if(window.matchMedia('(min-width: 768px)').matches) return true
     return false
   }
-  throw new Error('window가 아직 없음')
+  return true;
 }
 
 export default isDesktop
