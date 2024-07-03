fa1a0655233188766b72482c01f04ea97e30ec50
loevray <rei980213@gmail.com>
2024-07-03 15:06:28 +0900

    Feat: Toast Component

---
+++ src/app/ui/common/toast/Toast.tsx
@@ -0,0 +1,66 @@
+import { ReactNode, useRef } from "react";
+import CloseIcon from "./Icon/CloseIcon";
+import DecreaseProgress from "../progress/DecreaseProgress";
+import useHover from "@/hooks/useHover";
+
+interface ToastProps {
+  Icon: () => JSX.Element;
+  iconColor: string;
+  duration: number;
+  message: string;
+  onCloseButtonClick: (id: number) => void;
+  onComplete: (id: number) => void;
+  id: number;
+}
+
+const Toast = ({
+  duration,
+  Icon,
+  iconColor,
+  onCloseButtonClick,
+  onComplete,
+  id,
+  message,
+}: ToastProps) => {
+  const hoverRef = useRef<HTMLDivElement>(null);
+  const { isHover } = useHover({ hoverRef });
+  const onCloseButtonClickById = onCloseButtonClick.bind(null, id);
+  const onCompleteById = onComplete.bind(null, id);
+
+  return (
+    <div
+      className="w-30 rounded-lg overflow-hidden shadow-radial"
+      ref={hoverRef}
+    >
+      <div
+        id="toast-default"
+        className="flex items-center w-30 p-1.2 text-gray-500 bg-white   dark:text-gray-400 dark:bg-gray-800"
+        role="alert"
+      >
+        <div
+          className={`inline-flex items-center justify-center flex-shrink-0 w-3 h-3  ${iconColor}  rounded-lg `}
+        >
+          <Icon />
+        </div>
+        <div className="ms-3 text-1.4 text-black font-normal">{message}</div>
+        <button
+          type="button"
+          className="ms-auto mx-0.5 my-0.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-0.5 hover:bg-gray-100 inline-flex items-center justify-center h-2 w-2 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+          data-dismiss-target="#toast-default"
+          aria-label="Close"
+          onClick={onCloseButtonClickById}
+        >
+          <CloseIcon />
+        </button>
+      </div>
+      <DecreaseProgress
+        duration={duration}
+        colorSet={iconColor}
+        shouldPause={isHover}
+        onComplete={onCompleteById}
+      />
+    </div>
+  );
+};
+
+export default Toast;
--- src/app/ui/common/toast/ToastContainer.tsx
+++ src/app/ui/common/toast/ToastContainer.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { ToastEventType, observerInstance } from "./createObserver";
 import Portal from "../Portal/Portal";
-import CloseIcon from "./Icon/CloseIcon";
 import ToastDefaultIcon from "./Icon/ToastDefaultIcon";
 import ToastErrorIcon from "./Icon/ToastErrorIcon";
 import ToastSuccessIcon from "./Icon/ToastSuccessIcon";
 import ToastWarningIcon from "./Icon/ToastWarningIcon";
-import DecreaseProgress from "../progress/DecreaseProgress";
-interface Toast {
+import Toast from "./Toast";
+interface ToastType {
   id: number;
   message: string;
-  duration?: number;
+  duration: number;
   eventType: ToastEventType;
 }
 
 const ToastContainer = () => {
-  const [toasts, setToasts] = useState<Toast[]>([]);
+  const [toasts, setToasts] = useState<ToastType[]>([]);
+
   useEffect(() => {
     const handleAddToast = ({
       eventType,
@@ -26,7 +26,7 @@ const ToastContainer = () => {
     }: {
       eventType: ToastEventType;
       message: string;
-      duration?: number;
+      duration: number;
     }) => {
       const id = new Date().getTime();
 
@@ -48,67 +48,45 @@ const ToastContainer = () => {
   } = {
     default: {
       icon: ToastDefaultIcon,
-      color: "bg-blue-100",
+      color: "bg-blue-100 text-blue-500 dark:bg-blue-800 dark:text-blue-200",
     },
     warning: {
       icon: ToastWarningIcon,
-      color: "bg-orange-100",
+      color:
+        "text-orange-500 bg-orange-100 dark:bg-orange-700 dark:text-orange-200",
     },
     error: {
       icon: ToastErrorIcon,
-      color: "bg-red-100",
+      color: "text-red-500 bg-red-100 dark:bg-red-800 dark:text-red-200",
     },
     success: {
       icon: ToastSuccessIcon,
-      color: "bg-green-100",
+      color:
+        "text-green-500 bg-green-100  dark:bg-green-800 dark:text-green-200",
     },
+  };
+
+  const onCloseButtonClick = (id: number) => {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
   };
 
   return (
     <Portal>
       <div className="fixed top-0 right-0 z-[1000] m-4 flex flex-col gap-2">
-        {toasts.map((toast) => {
-          const IconComponent = mappingIcon[toast.eventType].icon;
-          const bg = mappingIcon[toast.eventType].color;
+        {toasts.map(({ eventType, id, duration, message }) => {
+          const IconComponent = mappingIcon[eventType].icon;
+          const iconColor = mappingIcon[eventType].color;
           return (
-            <div
-              className="w-30 rounded-lg overflow-hidden shadow-radial"
-              key={toast.id}
-            >
-              <div
-                id="toast-default"
-                className="flex items-center w-30 p-1.2 text-gray-500 bg-white   dark:text-gray-400 dark:bg-gray-800"
-                role="alert"
-              >
-                <div
-                  className={`inline-flex items-center justify-center flex-shrink-0 w-3 h-3 text-blue-500 ${bg}  rounded-lg dark:bg-blue-800 dark:text-blue-200`}
-                >
-                  <IconComponent />
-                </div>
-                <div className="ms-3 text-1.4 text-black font-normal">
-                  {toast.message}
-                </div>
-                <button
-                  type="button"
-                  className="ms-auto mx-0.5 my-0.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-0.5 hover:bg-gray-100 inline-flex items-center justify-center h-2 w-2 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-                  data-dismiss-target="#toast-default"
-                  aria-label="Close"
-                  onClick={() => {
-                    setToasts((prev) =>
-                      prev.filter((_toast) => _toast.id !== toast.id)
-                    );
-                  }}
-                >
-                  <CloseIcon />
-                </button>
-              </div>
-              <DecreaseProgress
-                duration={toast.duration}
-                onComplete={() =>
-                  setToasts((prev) => prev.filter(({ id }) => toast.id !== id))
-                }
-              />
-            </div>
+            <Toast
+              key={id}
+              id={id}
+              duration={duration}
+              Icon={IconComponent}
+              iconColor={iconColor}
+              message={message}
+              onCloseButtonClick={onCloseButtonClick}
+              onComplete={onCloseButtonClick}
+            />
           );
         })}
       </div>
