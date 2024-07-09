698a0d93a8403649a1f841025eaa071bebb06315
loevray <rei980213@gmail.com>
2024-07-09 16:15:34 +0900

    Refactor: Button component twMerged classname

--- src/app/ui/common/Button.tsx
+++ src/app/ui/common/Button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ButtonHTMLAttributes, HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
 type DefaultColors = "miku" | "rin";
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -20,9 +21,10 @@ const Button = ({
     miku: "bg-cyan-500 hover:bg-cyan-400 text-white disabled:cursor-not-allowed disabled:bg-cyan-600",
     rin: "bg-yellow-300 hover:bg-yellow-200 disabled:bg-yellow-600 disabled:cursor-not-allowed text-amber-950",
   };
+  const mergedClass = twMerge("w-10 h-3", className);
   return (
     <button
-      className={`${color[colorPalette]} w-10 h-3  ${className} cursor-pointer  rounded-sm  shadow-md`}
+      className={`${color[colorPalette]} ${mergedClass} cursor-pointer  rounded-sm  shadow-md`}
       {...rest}
     >
       {text ? text : children}
