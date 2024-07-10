2374220cd0c0b754431780e192a49b0a0d0c014f
loevray <rei980213@gmail.com>
2024-07-10 17:33:36 +0900

    Style: navigation styling

--- src/app/ui/navigation/Navigation.tsx
+++ src/app/ui/navigation/Navigation.tsx
@@ -16,6 +16,7 @@ export type RoutesType =
 export interface I_RoutesStyle {
   name: string;
   backgroundColor: string;
+  hoverColor: string;
   icon: React.ComponentType<React.ComponentProps<"svg">>;
 }
 export type MappedRoutesStyleType = {
@@ -32,56 +33,33 @@ const isCorrectRoute = (
 const Navigation = () => {
   const pathname = usePathname();
   const currentPath = pathname.split("/")[1];
-  const currentBackgroundColor = isCorrectRoute(currentPath, ROUTES.STYLE)
-    ? ROUTES.STYLE[currentPath].backgroundColor
-    : "md:bg-slate-100";
+  let pathColor = {
+    bg: "md:bg-slate-100",
+    hoverBg: "md:hover:bg-gray-200",
+  };
 
-  const [expanded, setExpanded] = useState<string | false>("accordion1");
-
-  const handleToggle =
-    (panel: string) => (e: SyntheticEvent, isExpanded: boolean) => {
-      setExpanded(isExpanded ? panel : false);
-    };
+  pathColor = isCorrectRoute(currentPath, ROUTES.STYLE)
+    ? {
+        bg: ROUTES.STYLE[currentPath].backgroundColor,
+        hoverBg: ROUTES.STYLE[currentPath].hoverColor,
+      }
+    : pathColor;
 
   return (
     <nav
-      className={`fixed md:sticky bottom-0 md:top-0 h-5 md:h-screen w-screen md:w-20 bg-white ${currentBackgroundColor} flex flex-col justify-between
+      className={`fixed md:rounded-r-[2rem] md:sticky bottom-0 md:top-0 h-5 md:h-screen w-screen md:w-20 bg-white ${pathColor.bg} flex flex-col justify-between
       `}
     >
-      <section className="flex h-full flex-col gap-0.5">
-        {/* desktop ui */}
-        <Accordion
-          expanded={expanded === "accordion1"}
-          handleToggle={handleToggle("accordion1")}
-          className="w-full hidden md:block"
-        >
-          <Accordion.Summary>유틸리티</Accordion.Summary>
-          {mappedRoutesStyleArray.map(([link, { name }]) => (
-            <Accordion.Content key={link} className="px-0">
-              <Link href={link} className="hover:bg-slate-100 block p-1">
-                {name}
-              </Link>
-            </Accordion.Content>
-          ))}
-        </Accordion>
-        <Accordion
-          expanded={expanded === "accordion2"}
-          handleToggle={handleToggle("accordion2")}
-          className="w-full hidden md:block"
-        >
-          <Accordion.Summary>메뉴2</Accordion.Summary>
-          <Accordion.Content className="px-0">
-            <Link href={"/lab-1"} className="hover:bg-slate-100 block p-1">
-              실험실 1
-            </Link>
-          </Accordion.Content>
-        </Accordion>
-
-        {/* mobile ui  */}
-        <div className="flex h-full justify-around items-center border-t-2 border-gray-100 md:hidden">
-          {mappedRoutesStyleArray.map(([link, { icon: Icon }]) => (
-            <Link href={link} key={link}>
-              <Icon className={`size-2.5`} />
+      <section className="flex h-1/2 flex-col gap-0.5">
+        <div className="flex md:flex-col h-full justify-around items-center  ">
+          {mappedRoutesStyleArray.map(([link, { icon: Icon, name }]) => (
+            <Link
+              className={`md:rounded-2xl md:flex md:w-full md:pl-1 items-center gap-1 ${pathColor.hoverBg} md:py-1`}
+              href={link}
+              key={link}
+            >
+              <Icon className={`size-2.5 md:size-3`} />
+              <span>{name}</span>
             </Link>
           ))}
         </div>
--- src/app/ui/navigation/constants/route.ts
+++ src/app/ui/navigation/constants/route.ts
@@ -9,28 +9,33 @@ import LadderIcon from "../LadderIcon";
   STYLE:{
     "/": {
       name: "메인",
-      backgroundColor: "md:bg-gray",
+      backgroundColor: "md:bg-slate-100",
+      hoverColor:'md:hover:bg-gray-200',
       icon:HomeIcon
       
     },
     "spin-wheel": {
       name: "룰렛",
       backgroundColor: "md:bg-cyan-300",
+      hoverColor:'md:hover:bg-cyan-400',
       icon: LifebuoyIcon
     },
     "youtube-comment-raffle": {
       name: "유튜브 댓글 추첨",
       backgroundColor: "md:bg-yellow-300",
+      hoverColor:'md:hover:bg-yellow-400',
       icon: PlayCircleIcon
     },
     "ghost-leg":{
       name:"사다리 타기",
       backgroundColor:"md:bg-pink-200",
+      hoverColor:'md:hover:bg-pink-300',
       icon:LadderIcon
     },
     "lab-1":{
       name:"실험실",
       backgroundColor:"md:bg-emerald-200",
+      hoverColor:'md:hover:bg-emerald-300',
       icon: BeakerIcon
     }
   },
