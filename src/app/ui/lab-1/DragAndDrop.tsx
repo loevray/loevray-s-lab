87d4669b908419a160b525350402e787339baf68
loevray <rei980213@gmail.com>
2024-07-24 16:25:12 +0900

    Feat: add utility function for swap array elements

--- src/app/ui/lab-1/DragAndDrop.tsx
+++ src/app/ui/lab-1/DragAndDrop.tsx
@@ -3,6 +3,9 @@
 import { useRef, useState } from "react";
 const initialItems = ["Item 1", "Item 2", "Item 3", "Item 4"];
 
+const swap = <T,>(arr: T[], index1: number, index2: number) =>
+  ([arr[index1], arr[index2]] = [arr[index2], arr[index1]]);
+
 const DragAndDrop = () => {
   const [items, setItems] = useState(initialItems);
   const draggingIndex = useRef(-1);
@@ -13,9 +16,10 @@ const DragAndDrop = () => {
 
   const onDragOver = (index: number) => {
     if (draggingIndex.current === -1 || draggingIndex.current === index) return;
+
     const newItems = [...items];
-    const [draggedItem] = newItems.splice(draggingIndex.current, 1);
-    newItems.splice(index, 0, draggedItem);
+
+    swap<string>(newItems, draggingIndex.current, index);
     draggingIndex.current = index;
     setItems(newItems);
   };
@@ -25,12 +29,13 @@ const DragAndDrop = () => {
   };
 
   return (
-    <div className="App">
-      <h2>Drag and Drop List</h2>
-      <ul>
+    <div>
+      <h2 draggable>Drag and Drop List</h2>
+      <ul className="flex bg-emerald-200 w-50 h-10 p-2 gap-3">
         {items.map((item, index) => (
           <li
-            key={item + index}
+            className="h-full flex-1 flex justify-center items-center bg-indigo-300 cursor-pointer"
+            key={`${item},${index}`}
             draggable
             onDragStart={() => onDragStart(index)}
             onDragOver={() => onDragOver(index)}
