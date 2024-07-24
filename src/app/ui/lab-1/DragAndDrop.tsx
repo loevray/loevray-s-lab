3d81934124e1be6dbde338b7d61a4069a188d3b7
loevray <rei980213@gmail.com>
2024-07-24 15:39:30 +0900

    Feat: drag and drop

--- src/app/ui/lab-1/DragAndDrop.tsx
+++ src/app/ui/lab-1/DragAndDrop.tsx
@@ -1,40 +1,46 @@
 "use client";
 
 import { useState } from "react";
+const initialItems = ["Item 1", "Item 2", "Item 3", "Item 4"];
 
 const DragAndDrop = () => {
-  const [draggableElements, setDraggableElements] = useState([
-    "요소1",
-    "요소2",
-    "요소3",
-    "요소4",
-    "요소5",
-  ]);
-  const handleDragStart = () => {};
-  const handleDrag = () => {};
-  const handleDragEnter = () => {};
-  const handleDragOver = () => {};
+  const [items, setItems] = useState(initialItems);
+  const [draggingIndex, setDraggingIndex] = useState(-1);
 
-  /* 
-    1. 요소를 드래그해서 움직이는 동안, 다른 요소와 자리가 겹칠 시 그 자리를 밀려나게해줌
-      1-1. 요소의 자리가 밀려나려면, key값을 다르게 주고 state를 사용해야한다.
-  */
+  const onDragStart = (index: number) => {
+    setDraggingIndex(index);
+  };
+
+  const onDragOver = (index: number) => {
+    if (draggingIndex === -1 || draggingIndex === index) return;
+    const newItems = [...items];
+    const [draggedItem] = newItems.splice(draggingIndex, 1);
+    newItems.splice(index, 0, draggedItem);
+    setDraggingIndex(index);
+    setItems(newItems);
+  };
+
+  const onDragEnd = () => {
+    setDraggingIndex(-1);
+  };
+
   return (
-    <ul className="*:bg-emerald-200 *:flex *:gap-2 *:items-center *:p-2 *:h-10 flex gap-1 mt-2">
-      <li className="*:w-10 *:h-full *:flex *:justify-center *:items-center *:cursor-pointer *:bg-gray-50">
-        {draggableElements.map(
-          (el, i) =>
-            i < 4 && (
-              <span draggable key={`${el},${i}`}>
-                {el}
-              </span>
-            )
-        )}
-      </li>
-      <li className="*:w-10 *:h-full *:flex *:justify-center *:items-center *:cursor-pointer *:bg-gray-50">
-        <span draggable>{draggableElements[draggableElements.length - 1]}</span>
-      </li>
-    </ul>
+    <div className="App">
+      <h2>Drag and Drop List</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li
+            key={item + index}
+            draggable
+            onDragStart={() => onDragStart(index)}
+            onDragOver={() => onDragOver(index)}
+            onDragEnd={onDragEnd}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
